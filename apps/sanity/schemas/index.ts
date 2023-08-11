70242fcd0839985e460ba7f79e376776720de318
Raivis Kārkliņš <74545919+designer-rxk@users.noreply.github.com>
2023-08-11 23:36:43 +0300

    Add basic sanity schemas

--- apps/sanity/schemas/index.ts
+++ apps/sanity/schemas/index.ts
@@ -1,1 +1,6 @@
-export const schemaTypes = []
+import blockContent from './components/blockContent'
+import category from './components/category'
+import post from './components/post'
+import author from './components/author'
+
+export const schemaTypes = [post, author, category, blockContent]
