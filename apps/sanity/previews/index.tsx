b0978ef58289114a6a261797cbb808dedb91eaf4
Raivis Kārkliņš <74545919+designer-rxk@users.noreply.github.com>
2023-08-15 22:28:05 +0300

    Add sanity preview

---
+++ apps/sanity/previews/index.tsx
@@ -0,0 +1,10 @@
+import { ComponentView } from 'sanity/desk'
+export { WebPreview } from './WebPreview'
+
+export const JsonView: React.ComponentType<ComponentView> = () => {
+  return (
+    <div>
+        This is a JSON view
+    </div>
+    )
+}
