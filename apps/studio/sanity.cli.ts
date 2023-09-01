932572f1ed6c6fdd324d75b051109ae792b3f0e9
Raivis Kārkliņš <74545919+designer-rxk@users.noreply.github.com>
2023-09-01 14:01:36 +0300

    Add sanity runtime to .gitignore

--- apps/studio/sanity.cli.ts
+++ apps/studio/sanity.cli.ts
@@ -1,8 +1,8 @@
-import {defineCliConfig} from 'sanity/cli'
+import { defineCliConfig } from 'sanity/cli';
 
 export default defineCliConfig({
   api: {
     projectId: 'ww63veng',
-    dataset: 'production'
-  }
-})
+    dataset: 'production',
+  },
+});
